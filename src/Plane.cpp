dbf5ee36e575db2b69e582aa6621f28abc570866
dizzybumblebee <emmaleonamueller@outlook.com>
2025-09-26 19:03:41 +0200

    Completed intersect function in Plane.cpp

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -36,14 +36,27 @@ intersect(const Ray& _ray,
           double&    _intersection_t ) const
 {
 
-/** \todo
- * - compute the intersection of the plane with `_ray`
- * - if ray and plane are parallel there is no intersection
- * - otherwise compute intersection data and store it in `_intersection_point`, `_intersection_normal`, and `_intersection_t`.
- * - return whether there is an intersection in front of the viewer (t > 0)
-*/
+        const vec3 &dir = _ray.direction;
+        const vec3   oc = _ray.origin - center;
 
-    return false;
+        //compute the intersection of the plane with `_ray`
+        double denom = dot(_intersection_normal, dir);
+
+        //check if ray is parallel to plane
+        if (fabs(denom) < 0.000000001) return false;
+
+        double t = -dot(_intersection_normal,oc) / denom;
+
+        //check if ray intersects plane behind origin
+        if (t <= 0) return false;
+
+        _intersection_t      = t;
+        _intersection_point  = _ray.origin + t * dir;
+
+        //normal at intersection point is simply plane's normal
+        _intersection_normal = normal;
+
+        return true;
 }
 
 
